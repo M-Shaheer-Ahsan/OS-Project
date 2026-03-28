eb8e501eddc5a3395c49d47f5d9390b03be80763
M-Shaheer-Ahsan <shaheerahsan17@gmail.com>
2026-03-28 07:59:33 +0500

    completion of chunk_file func

--- src/chunker.c
+++ src/chunker.c
@@ -30,6 +30,38 @@ int chunk_file(const char *filename, Chunk **chunks, int *chunk_count) {
         return -1;
     }
 
+    for (int i = 0; i < *chunk_count; i++) {
+        size_t bytes_to_read = CHUNK_SIZE;
+        if (i == *chunk_count - 1)
+            bytes_to_read = file_size - ((long)i * CHUNK_SIZE);
+
+        (*chunks)[i].data = (unsigned char *)malloc(bytes_to_read);
+        if (!(*chunks)[i].data) {
+            fprintf(stderr, "Error: memory allocation failed for chunk %d\n", i);
+            for (int j = 0; j < i; j++) free((*chunks)[j].data);
+            free(*chunks);
+            fclose(file);
+            return -1;
+        }
+
+        size_t bytes_read = fread((*chunks)[i].data, 1, bytes_to_read, file);
+        if (bytes_read != bytes_to_read) {
+            fprintf(stderr, "Error: could not read chunk %d\n", i);
+            for (int j = 0; j <= i; j++) free((*chunks)[j].data);
+            free(*chunks);
+            fclose(file);
+            return -1;
+        }
+
+        (*chunks)[i].id = i;
+        (*chunks)[i].original_size = bytes_read;
+        (*chunks)[i].compressed_size = 0;
+        (*chunks)[i].priority = i;
+    }
+
+    fclose(file);
+    printf("File split into %d chunk(s) of up to 1MB each\n", *chunk_count);
+
     return 0;
 }
 
